e84eb81feca5f636bbeee892360617cb26d6722d
manshisinghds0123-jpg <manshisinghds0123@gmail.com>
2026-07-23 20:33:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+CORS(app)
+
+model = joblib.load("heart_disease_model.pkl")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.json
+
+    features = np.array([
+        data["age"],
+        data["sex"],
+        data["cp"],
+        data["trestbps"],
+        data["chol"],
+        data["fbs"],
+        data["restecg"],
+        data["thalach"],
+        data["exang"],
+        data["oldpeak"],
+        data["slope"],
+        data["ca"],
+        data["thal"]
+    ]).reshape(1, -1)
+
+    prediction = model.predict(features)
+
+    if prediction[0] == 1:
+        result = "Heart Disease Detected"
+    else:
+        result = "No Heart Disease"
+
+    return jsonify({"prediction": result})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
